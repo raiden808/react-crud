99386910435590f194ea3716a3907343e7a18e66
raiden808 <raiden.nalasa@gmail.com>
2019-05-28 11:38:24 +0800

    applied apps js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Table from './App'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello, React!</h1>
+      <div className="container">
+        <Table />
       </div>
     )
   }
