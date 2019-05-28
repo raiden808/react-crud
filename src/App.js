14309d7c38c2263b015ea6437837c85e280e3eab
raiden808 <raiden.nalasa@gmail.com>
2019-05-29 06:44:22 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import Table from './table'
 
 class App extends Component {
   render() {
+
+  	const characters = [
+  		{
+  			name: 'Charlie',
+  			job: 'Janitor',
+  		},
+  		{
+  			name: 'Mac',
+  			job: 'Bouncer',
+  		},
+  		{
+  			name: 'Dee',
+  			job: 'Aspiring actress',
+  		},
+  		{
+  			name: 'Dennis',
+  			job: 'Bartender',
+  		},
+  	]
+
     return (
       <div className="container">
         <Table />
