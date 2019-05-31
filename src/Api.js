971d4aeefa9fbdc6c874ee08cb3610e4f1a045b7
raiden808 <raiden.nalasa@gmail.com>
2019-05-31 14:01:19 +0800

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -8,26 +8,26 @@ class App extends Component {
   // Code is invoked after the component is mounted/inserted into the DOM tree.
   componentDidMount() {
     const url =
-      'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*'
+    'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*'
 
     fetch(url)
-      .then(result => result.json())
-      .then(result => {
-        this.setState({
-          data: result,
-        })
-      })
-  }
+      	.then(result => result.json())
+      	.then(result => {
+        	this.setState({
+          		data: result,
+        	})
+      	})
+  	}
 
-  render() {
-    const { data } = this.state
+  	render() {
+	    const { data } = this.state
 
-    const result = data.map((entry, index) => {
-      return <li key={index}>{entry}</li>
-    })
+	    const result = data.map((entry, index) => {
+			return <li key={index}>{entry}</li>
+	    })
 
-    return <ul>{result}</ul>
-  }
+	    return <ul>{result}</ul>
+	}
 }
 
 export default App
